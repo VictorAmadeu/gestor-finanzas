d98507ea1f9017de13da249e46d1e8cbb7ca711e
Victor Amadeu Braga Heleno <127600481+VictorAmadeu@users.noreply.github.com>
2025-06-22 22:46:02 +0200

    Configurar TailwindCSS: configuración e integración inicial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 // src/App.js
-
 function App() {
-  return <div>Hola Mundo - Gestor de Finanzas Personales</div>;
+  return (
+    <div className="text-center bg-blue-100 p-10">
+      <h1 className="text-2xl font-bold text-blue-700">¡Hola, mundo!</h1>
+      <p className="mt-4 text-gray-600">
+        Proyecto Gestor de Finanzas Personales
+      </p>
+    </div>
+  );
 }
 
 export default App;
